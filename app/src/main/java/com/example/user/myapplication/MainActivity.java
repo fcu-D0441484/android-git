e21b1d4b6056121426fd99ad387faf9128e837c5
User_pc <dodoguit91095@gmail.com>
2017-05-25 20:25:58 +0800

    小組APP轉移

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +23,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        ListView list = (ListView)findViewById(R.id.LV);
+        ArrayList alist=new ArrayList<Pet_Item>();
+        Pet_Item item1=new Pet_Item(R.drawable.stray_pet,"浪浪資訊");
+        Pet_Item item2=new Pet_Item(R.drawable.pet_item,"製作寵物周邊");
+        Pet_Item item3=new Pet_Item(R.drawable.pet_background,"寵物桌布");
+        alist.add(item1);
+        alist.add(item2);
+        alist.add(item3);
+        Pet_adapter adapter = new Pet_adapter(this,alist);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(itemclick);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,7 +43,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    AdapterView.OnItemClickListener itemclick= new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            int [] imageIds=null;
+            Intent intent = new Intent();
+            switch(position)
+            {
+                case 0:
+                    imageIds = new int[1];
+                    imageIds[0]=R.drawable.stray_pet;
+                    intent.setClass(MainActivity.this,Pet_strayData.class);
+                    intent.putExtra("img_id1",imageIds);
+                    startActivity(intent);
+                    break;
+                case 1:
+                    imageIds=new int [1];
+                    imageIds[0]=R.drawable.pet_item;
+                    intent.setClass(MainActivity.this,Pet_produce_select.class);
+                    intent.putExtra("img_id2",imageIds);
+                    startActivity(intent);
+                    break;
+                case 2:
+                    imageIds= new int [1];
+                    imageIds[0]=R.drawable.pet_background;
+                    Uri uri = Uri.parse("https://wallpaper.gamme.com.tw/wp/category/pets/page2");
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setData(uri);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
